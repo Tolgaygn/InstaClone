8b7e3dedc01a14ba266aeb57260c36b6c4af091d
Tolga <tolgaaygun_35@hotmail.com>
2021-12-08 15:24:46 +0300

    first commit

---
+++ InstaClone/HomeCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeCell.swift
+//  InstaClone
+//
+//  Created by Tolga on 13.06.2021.
+//
+
+import UIKit
+
+class HomeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ InstaClone/UploadViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UploadViewController.swift
+//  InstaClone
+//
+//  Created by Tolga on 7.06.2021.
+//
+
+import UIKit
+
+class UploadViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
